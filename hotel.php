a52064f3f21467de1fae805b930152f0eacee001
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-22 14:30:19 +0100

    first commit

---
+++ hotel.php
@@ -0,0 +1,66 @@
+<?php 
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+// var_dump($hotels);
+var_dump($hotels[0],)
+
+?>
+
+
+
+<?php 
+for ($i = 0; $i < count($hotels); $i++ ) {
+    var_dump($hotels[$i]);
+?>
+<ul>
+    <?php
+    
+
+        // <li>
+        // </li>
+    ?>
+</ul>
+
+<?php 
+}
+
+?>
